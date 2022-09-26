6eaf8ab152333a67f287e49a6e4111c350b82648
Fabián Villena <villenafabian@gmail.com>
2022-09-26 11:16:54 -0300

    token

--- app/main.py
+++ app/main.py
@@ -4,8 +4,147 @@ import pydantic
 import deepclaim_ws.data
 import deepclaim_ws.dummy_classifier
 import typing
+import passlib.context
+import datetime
+import jose.jwt
+import jose
+import dotenv
+import json
+
+env = dotenv.dotenv_values("secret.env")
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = env["SECRET_KEY"]
+ALGORITHM = env["ALGORITHM"]
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+with open('users.json') as f:
+    clients_db = json.load(f)
+
+class Token(pydantic.BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(pydantic.BaseModel):
+    client_id: typing.Union[str, None] = None
+
+
+class Client(pydantic.BaseModel):
+    client_id: str
+    disabled: typing.Union[bool, None] = None
+
+
+class ClientInDB(Client):
+    hashed_client_secret: str
+    
+class ClientIn(pydantic.BaseModel):
+    client_id: str
+    client_secret: str
+
+
+pwd_context = passlib.context.CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = fastapi.security.OAuth2PasswordBearer(tokenUrl="token2")
+
 
 app = fastapi.FastAPI(title="DeepClaim", description="API para comunicarse con el servicio de predicción de DeepClaim.")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_client(db, client_id: str):
+    if client_id in db:
+        user_dict = db[client_id]
+        return ClientInDB(**user_dict)
+
+
+def authenticate_user(db, client_id: str, client_secret: str):
+    client = get_client(db, client_id)
+    if not client:
+        return False
+    if not verify_password(client_secret, client.hashed_client_secret):
+        return False
+    return client
+
+
+def create_access_token(data: dict, expires_delta: typing.Union[datetime.timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jose.jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_client(token: str = fastapi.Depends(oauth2_scheme)):
+    credentials_exception = fastapi.HTTPException(
+        status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jose.jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        client_id: str = payload.get("sub")
+        if client_id is None:
+            raise credentials_exception
+        token_data = TokenData(client_id=client_id)
+    except jose.JWTError:
+        raise credentials_exception
+    user = get_client(clients_db, client_id=token_data.client_id)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_client(current_client: Client = fastapi.Depends(get_current_client)):
+    if current_client.disabled:
+        raise fastapi.HTTPException(status_code=400, detail="Inactive user")
+    return current_client
+
+@app.post("/token2", response_model=Token)
+async def login_for_access_token(form_data: fastapi.security.OAuth2PasswordRequestForm = fastapi.Depends()):
+    user = authenticate_user(clients_db, form_data.username, form_data.password)
+    if not user:
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.client_id}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.post("/token", response_model=deepclaim_ws.data.Token)
+async def login_for_access_token(form_data: ClientIn = fastapi.Depends()):
+    client = authenticate_user(clients_db, form_data.client_id, form_data.client_secret)
+    if not client:
+        raise fastapi.HTTPException(
+            status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": client.client_id}, expires_delta=access_token_expires
+    )
+    response = deepclaim_ws.data.Token(
+        TransactionID=0,
+        Status = deepclaim_ws.data.StatusClass(
+            Code = 200,
+            Value = "OK"
+        ),
+        accessToken=access_token
+    )
+    return response
 
 async def checker(datos_reclamo: str = fastapi.Form(...)):
     try:
@@ -14,7 +153,7 @@ async def checker(datos_reclamo: str = fastapi.Form(...)):
     except pydantic.ValidationError as e:
         raise fastapi.exceptions.HTTPException(detail=fastapi.encoders.jsonable_encoder(e.errors()), status_code=fastapi.status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-@app.post("/reclamo", response_model=deepclaim_ws.data.ReclamoOut, summary="Clasificar reclamo.", description="Este servicio recibe los datos del reclamo.")
+@app.post("/reclamo", response_model=deepclaim_ws.data.ReclamoOut, summary="Clasificar reclamo.", description="Este servicio recibe los datos del reclamo.", dependencies=[fastapi.Depends(get_current_active_client)])
 async def classify_reclamo(
     datos_reclamo: deepclaim_ws.data.JsonReclamo = fastapi.Depends(checker), 
     archivo_1: typing.Optional[fastapi.UploadFile] = fastapi.File(None), 
@@ -31,18 +170,20 @@ async def classify_reclamo(
             Code = 200,
             Value = "OK"
         ),
-        Respuesta = deepclaim_ws.data.RespuestaClass(
-            Id_caso = datos_reclamo.Id_caso,
-            Mercado = c_mercado.predict([datos_reclamo.descripcion_problema])[0],
-            Tipo_entidad = c_tipo_entidad.predict([datos_reclamo.descripcion_problema])[0],
-            Nombre_entidad = c_nombre_entidad.predict([datos_reclamo.descripcion_problema])[0],
-            Tipo_producto = c_tipo_producto.predict([datos_reclamo.descripcion_problema])[0],
-            Tipo_materia = c_tipo_materia.predict([datos_reclamo.descripcion_problema])[0]
+        Data = deepclaim_ws.data.RespuestaClass(
+            id_caso = datos_reclamo.id_caso,
+            mercado = deepclaim_ws.data.MercadoClass(
+                tipo_mercado = c_mercado.predict([datos_reclamo.descripcion_problema])[0],
+                tipo_entidad = c_tipo_entidad.predict([datos_reclamo.descripcion_problema])[0],
+                nombre_entidad = c_nombre_entidad.predict([datos_reclamo.descripcion_problema])[0],
+                tipo_producto = c_tipo_producto.predict([datos_reclamo.descripcion_problema])[:2],
+                tipo_materia = c_tipo_materia.predict([datos_reclamo.descripcion_problema])[:2]
+            )
         )
     )
     return response
 
-@app.post("/reclamo_diario", response_model=deepclaim_ws.data.ReclamoDiarioOut, summary="Clasificar reclamos diarios.", description="Este servicio recibe los datos del reclamo ingresado por el ciudadano con los datos del día anterior con el fin de poder realizar cuadraturas al servicio.")
+@app.post("/reclamo_diario", response_model=deepclaim_ws.data.ReclamoDiarioOut, summary="Clasificar reclamos diarios.", description="Este servicio recibe los datos del reclamo ingresado por el ciudadano con los datos del día anterior con el fin de poder realizar cuadraturas al servicio.", dependencies=[fastapi.Depends(get_current_active_client)])
 async def classify_reclamo_diario(
     datos_reclamo: deepclaim_ws.data.JsonReclamos
     ):  
@@ -55,7 +196,7 @@ async def classify_reclamo_diario(
     )
     return response
 
-@app.post("/reclamo_sinRespuesta", response_model=deepclaim_ws.data.ReclamoOut, summary="Clasificar reclamo.", description="Este servicio recibe los datos del reclamo que no han tenido clasificación por su equipo, este cliente se activará 4 veces al día y enviará los datos del reclamocomo si se tratará del servicio Reclamo pero de manera diferida")
+@app.post("/reclamo_sinRespuesta", response_model=deepclaim_ws.data.ReclamoOut, summary="Clasificar reclamo.", description="Este servicio recibe los datos del reclamo que no han tenido clasificación por su equipo, este cliente se activará 4 veces al día y enviará los datos del reclamocomo si se tratará del servicio Reclamo pero de manera diferida", dependencies=[fastapi.Depends(get_current_active_client)])
 async def classify_reclamo_sinRespuesta(
     datos_reclamo: deepclaim_ws.data.Json = fastapi.Depends(checker), 
     archivo_1: typing.Optional[fastapi.UploadFile] = fastapi.File(None), 
@@ -83,7 +224,7 @@ async def classify_reclamo_sinRespuesta(
     )
     return response
 
-@app.post("/reclamo_retro", response_model=deepclaim_ws.data.ReclamoRetroOut, summary="Clasificar reclamos diarios.", description="Este servicio recibe los datos del reclamo con la clasificación ingresada por los analistas de CMF, con el fin de que puedan realizar retroalimentación a la clasificación proporcionada por el algoritmo.")
+@app.post("/reclamo_retro", response_model=deepclaim_ws.data.ReclamoRetroOut, summary="Clasificar reclamos diarios.", description="Este servicio recibe los datos del reclamo con la clasificación ingresada por los analistas de CMF, con el fin de que puedan realizar retroalimentación a la clasificación proporcionada por el algoritmo.", dependencies=[fastapi.Depends(get_current_active_client)])
 async def receive_reclamo_retro(
     datos_clasificación: deepclaim_ws.data.JsonClasificacion
     ):  
--- deepclaim_ws/data.py
+++ deepclaim_ws/data.py
@@ -4,7 +4,7 @@ import datetime
 import typing
 
 class JsonReclamo(pydantic.BaseModel):
-    Id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención")
+    id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención")
     fecha_ingreso: datetime.date = fastapi.Query(default=None, description="Fecha ingreso")
     descripcion_problema: str = fastapi.Query(default=None, description="Descripción del problema ingresado por el ciudadano")
     peticion_solicitud: str = fastapi.Query(default=None, description="Petición del problema ingresado por el ciudadano")
@@ -16,25 +16,28 @@ class StatusClass(pydantic.BaseModel):
     Code: int = fastapi.Query(default=None, description="Códigos de estado de Respuesta HTML (¿HTTP querían decir?)")
     Value: str = fastapi.Query(default=None, description="Descripción del Código")
 
+class MercadoClass(pydantic.BaseModel):
+    tipo_mercado: str = fastapi.Query(default=None, description="Tipo de mercado: Seguros-bancos-valores")
+    tipo_entidad: str = fastapi.Query(default=None, description="Tipo de entidad identificada ( desde el listado de entidades de la CMF)")
+    nombre_entidad: str = fastapi.Query(default=None, description="Nombre de entidad identificada ( desde el listado de entidades de la CMF)")
+    tipo_producto: typing.List[str] = fastapi.Query(default=None, description="Tipo producto identificados ( desde listado de tipos productos de la CMF)")
+    tipo_materia: typing.List[str] = fastapi.Query(default=None, description="Tipo materia identificados ( desde listado de tipos materia de la CMF)")
+
 class RespuestaClass(pydantic.BaseModel):
-    Id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención(enviado en la consulta)")
-    Mercado: str = fastapi.Query(default=None, description="Tipo de mercado: Seguros-bancos-valores")
-    Tipo_entidad: str = fastapi.Query(default=None, description="Tipo de entidad identificada ( desde el listado de entidades de la CMF)")
-    Nombre_entidad: str = fastapi.Query(default=None, description="Nombre de entidad identificada ( desde el listado de entidades de la CMF)")
-    Tipo_producto: str = fastapi.Query(default=None, description="Tipo producto identificados ( desde listado de tipos productos de la CMF)")
-    Tipo_materia: str = fastapi.Query(default=None, description="Tipo materia identificados ( desde listado de tipos materia de la CMF)")
+    id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención(enviado en la consulta)")
+    mercado: MercadoClass  
 
 class ReclamoOut(pydantic.BaseModel):
-    TransactionID: int = fastapi.Query(default=None, description="Código Único de la transacción")
+    TransactionID: str = fastapi.Query(default=None, description="Código Único de la transacción")
     Status: StatusClass
-    Respuesta:  RespuestaClass
+    Data:  RespuestaClass
 
 class DocumentosClass(pydantic.BaseModel):
     hash_archivo: str = fastapi.Query(default=None, description="Hash registrado por el archivo")
     nombre_archivo: str = fastapi.Query(default=None, description="Nombre archivo con extensión")
 
 class DatosCasos(pydantic.BaseModel):
-    Id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención")
+    id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención")
     fecha_ingreso: datetime.date = fastapi.Query(default=None, description="Fecha ingreso")
     descripcion_problema: str = fastapi.Query(default=None, description="Descripción del problema ingresado por el ciudadano")
     peticion_solicitud: str = fastapi.Query(default=None, description="Petición del problema ingresado por el ciudadano")
@@ -55,22 +58,25 @@ class ReclamoDiarioOut(pydantic.BaseModel):
     Status: StatusClass
 
 class Json(pydantic.BaseModel):
-    Id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención")
+    id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención")
     fecha_ingreso: datetime.date = fastapi.Query(default=None, description="Fecha ingreso")
     descripcion_problema: str = fastapi.Query(default=None, description="Descripción del problema ingresado por el ciudadano")
     peticion_solicitud: str = fastapi.Query(default=None, description="Petición del problema ingresado por el ciudadano")
     clasificacion_ciudadano: str = fastapi.Query(default=None, description="Producto - clasificación inicial ingresada por el ciudadano")
     entidad_ciudadano: str = fastapi.Query(default=None, description="Solo para los casos bancarios ingresan el banco contra el cual está reclamando")
     cantidad_documentos: int = fastapi.Query(default=None, description="Se indicará la cantidad de documentos que se enviaran como adjuntos")
+    descripcion_error: str = fastapi.Query(default=None, description="Se indicará la descripción del error, vacío en caso de tener error")
     
 class JsonClasificacion(pydantic.BaseModel):
-    Id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención(enviado en la consulta)")
-    Mercado: str = fastapi.Query(default=None, description="Tipo de mercado: Seguros-bancos-valores")
-    Tipo_entidad: str = fastapi.Query(default=None, description="Tipo de entidad identificada ( desde el listado de entidades de la CMF)")
-    Nombre_entidad: str = fastapi.Query(default=None, description="Nombre de entidad identificada ( desde el listado de entidades de la CMF)")
-    Tipo_producto: str = fastapi.Query(default=None, description="Tipo producto identificados ( desde listado de tipos productos de la CMF)")
-    Tipo_materia: str = fastapi.Query(default=None, description="Tipo materia identificados ( desde listado de tipos materia de la CMF)")
+    id_caso: int = fastapi.Query(default=None, description="ID único interno del ticket de atención(enviado en la consulta)")
+    mercado: MercadoClass
     
 class ReclamoRetroOut(pydantic.BaseModel):
     TransactionID: int = fastapi.Query(default=None, description="Código Único de la transacción")
     Status: StatusClass
+    
+
+class Token(pydantic.BaseModel):
+    TransactionID: str = fastapi.Query(default=None, description="Código Único de la transacción")
+    Status: StatusClass
+    accessToken: str
